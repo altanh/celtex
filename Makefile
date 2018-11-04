023fea80ebaba7f84a063b63d329a109874ddf8a
Altan Haan <altanh@cs.washington.edu>
2018-11-04 00:35:15 -0700

    Now using SDL2 for rendering.

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 CC=g++
-CPPFLAGS=-std=c++11 -O2 -Wall
+CPPFLAGS=-std=c++11 -O2 -Wall `sdl2-config --cflags`
 SDIR=src
 ODIR=obj
+LIBS=`sdl2-config --libs`
 
 _DEPS = Cell.h Grid.h Updater.h Pattern.h Automaton.h
 DEPS = $(patsubst %,$(SDIR)/%,$(_DEPS))
@@ -10,10 +11,10 @@ _OBJ = Cell.o Grid.o Updater.o Pattern.o Automaton.o celtex.o
 OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
 
 $(ODIR)/%.o: $(SDIR)/%.cc $(DEPS)
-	$(CC) -c -o $@ $< $(CPPFLAGS)
+	$(CC) -c -o $@ $< $(CPPFLAGS) $(LIBS)
 
 celtex: $(OBJ)
-	$(CC) -o $@ $^ $(CPPFLAGS)
+	$(CC) -o $@ $^ $(CPPFLAGS) $(LIBS)
 
 .PHONY: clean
 
--- src/celtex.cc
+++ src/celtex.cc
@@ -1,13 +1,42 @@
 #include <iostream>
+#include <cstdlib>
 #include <memory>
+#include <SDL.h>
 
 #include "Grid.h"
 #include "Automaton.h"
 #include "Updater.h"
 #include "Pattern.h"
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 int main(int argc, char **argv) {
-  Automaton aut(32, 32, true, std::shared_ptr<Updater>(new StochasticGOL()));
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    std::cerr << SDL_GetError() << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  SDL_Window *win = SDL_CreateWindow("celtex | automata based texture generation", 
+      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+  if (!win) {
+    std::cerr << SDL_GetError() << std::endl;
+    SDL_Quit();
+
+    return EXIT_FAILURE;
+  }
+
+  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+  if (!ren) {
+    SDL_DestroyWindow(win);
+    std::cerr << SDL_GetError() << std::endl;
+    SDL_Quit();
+
+    return EXIT_FAILURE;
+  }
+
+  Automaton aut(WIDTH, HEIGHT, true, std::shared_ptr<Updater>(new GOL()));
 
   Pattern glider(3, 3, {
     { false }, {  true }, { false },
@@ -21,22 +50,43 @@ int main(int argc, char **argv) {
     { false }, {  true }, { false }
   });
 
-  aut.putPattern(14, 14, pentomino);
+  aut.putPattern(WIDTH / 2, HEIGHT / 2, pentomino);
 
-  while (true) {
+  SDL_Event e;
+  bool running = true;
+  while (running) {
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+        case SDL_QUIT:
+          running = false;
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    SDL_RenderClear(ren);
+
     const Grid &g = aut.getGrid();
 
     for (size_t y = 0; y < g.getHeight(); ++y) {
       for (size_t x = 0; x < g.getWidth(); ++x) {
-        std::cout << (g.getCell(x,y).state ? "*" : " ");
+        bool alive = g.getCell(x, y).state;
+
+        SDL_SetRenderDrawColor(ren, alive ? 255 : 0, 0, 0, 255);
+        SDL_RenderDrawPoint(ren, x, y);
       }
-      std::cout << std::endl;
     }
 
-    aut.step();
+    SDL_RenderPresent(ren);
 
-    std::cin.get();
+    aut.step();
   }
+
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
 
   return 0;
 }
